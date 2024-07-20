fa167f6f99419beeefb736dddf8fbe35f50e1472
JonIriarte <61544049+JonIriarte@users.noreply.github.com>
2024-07-20 13:49:20 +0200

    project files

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/src/presentation/screens/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Center(child: Text('Hola Mundo')));
+    return const MaterialApp(
+        debugShowCheckedModeBanner: true, home: CounterScreen());
   }
 }
