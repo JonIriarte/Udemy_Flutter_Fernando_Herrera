918a4c64c97aaead69810b2520b8390c6c652b14
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-08 14:52:29 +0200

    Videos 92 to 96

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,16 +10,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme:  AppTheme().getTheme(),
-      title: 'Tok Tik',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tok Tik'),
-        ),
-        body: const Center(
-          child: Text('Hello Tok Tik'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DiscoverProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        title: 'Tok Tik',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tok Tik'),
+          ),
+          body: const Center(
+            child: Text('Hello,  Tok Tik'),
+          ),
         ),
       ),
     );
---
+++ toktik/lib/shared/data/local_video_post.dart
@@ -0,0 +1,45 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Escaleras',
+    'videourl': 'assets/videos/1.mp4',
+    'likes': 122,
+    'views': 1235
+  },
+  {
+    'name': 'Cascada',
+    'videourl': 'assets/videos/2.mp4',
+    'likes': 10000,
+    'views': 666665
+  },
+  {
+    'name': 'Catarata',
+    'videourl': 'assets/videos/3.mp4',
+    'likes': 6667,
+    'views': 444444
+  },
+  {
+    'name': 'Cascada',
+    'videourl': 'assets/videos/4.mp4',
+    'likes': 42,
+    'views': 42
+  },
+  {
+    'name': 'Catarata',
+    'videourl': 'assets/videos/5.mp4',
+    'likes': 456,
+    'views': 10
+  },
+  {
+    'name': 'Cascada',
+    'videourl': 'assets/videos/6.mp4',
+    'likes': 012345,
+    'views': 06
+  },
+  {'name': 'SIETE', 'videoUrl': 'assets/videos/7.mp4', 'likes': 12, 'views': 3},
+  {
+    'name': 'Pepperoni Pizza',
+    'videoUrl': 'assets/videos/8.mp4',
+    'likes': 99999,
+    'views': 0
+  }
+];
