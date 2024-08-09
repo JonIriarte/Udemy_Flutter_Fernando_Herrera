d199039779fe007974aafcb4637baf916a72c76d
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-09 12:25:06 +0200

    101: feat: Icons buttons on videos

---
+++ toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/human_formats.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class VideoButtons extends StatelessWidget {
+  const VideoButtons({super.key, required this.video});
+
+  final VideoPost video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      _CustomIconButton(
+          value: video.likes, icon: Icons.favorite, iconColor: Colors.red),
+      const SizedBox(
+        height: 15,
+      ),
+      _CustomIconButton(
+          value: video.likes,
+          icon: Icons.remove_red_eye_outlined,
+          iconColor: null),
+      const SizedBox(
+        height: 15,
+      ),
+      const _CustomIconButton(value: 0, icon: Icons.play_circle_outline)
+    ]);
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  const _CustomIconButton({required this.value, required this.icon, iconColor})
+      : color = iconColor ?? Colors.white;
+
+  final int value;
+  final IconData icon;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      IconButton(
+          onPressed: onPressedLike, icon: Icon(icon, color: color, size: 29)),
+      value > 0
+          ? Text(HumanFormats.humanReadableNumber(value.toDouble()))
+          : const Text('')
+    ]);
+  }
+
+  void onPressedLike() {
+    print('Like button pressed');
+  }
+}
--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -8,15 +9,20 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
-      children: [
-        Container(color: Colors.red[100]),
-        Container(color: Colors.green[100]),
-        Container(color: Colors.deepPurple[100]), 
-         Container(color: Colors.deepPurpleAccent[400])
-  
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          //Return video player + gradient + buttons
+          children: [
+            Positioned(
+                bottom: 40, right: 22, child: VideoButtons(video: videoPost))
+          ],
+        );
+      },
     );
   }
 }
