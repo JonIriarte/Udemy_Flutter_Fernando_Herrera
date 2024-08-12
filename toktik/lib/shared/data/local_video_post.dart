13bf17524ce8fb1b8ccc9bc0915c9f96238921f6
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-12 10:36:09 +0200

    FIx: Change video captions

--- toktik/lib/shared/data/local_video_post.dart
+++ toktik/lib/shared/data/local_video_post.dart
@@ -1,41 +1,46 @@
 List<Map<String, dynamic>> videoPosts = [
   {
-    'name': 'Escaleras',
+    'name': 'Perrete Akita Inu',
     'videoUrl': 'assets/videos/1.mp4',
     'likes': 122,
     'views': 1235
   },
   {
-    'name': 'Cascada',
+    'name': 'Night city lights',
     'videoUrl': 'assets/videos/2.mp4',
     'likes': 10000,
     'views': 666665
   },
   {
-    'name': 'Catarata',
+    'name': 'Lago de montaña',
     'videoUrl': 'assets/videos/3.mp4',
     'likes': 6667,
     'views': 444444
   },
   {
-    'name': 'Cascada',
+    'name': 'Saltar a la comba',
     'videoUrl': 'assets/videos/4.mp4',
     'likes': 42,
     'views': 42
   },
   {
-    'name': 'Catarata',
+    'name': 'Café imposible',
     'videoUrl': 'assets/videos/5.mp4',
     'likes': 456,
     'views': 10
   },
   {
-    'name': 'Cascada',
+    'name': 'Escaleras mecánicas',
     'videoUrl': 'assets/videos/6.mp4',
     'likes': 012345,
     'views': 06
   },
-  {'name': 'SIETE', 'videoUrl': 'assets/videos/7.mp4', 'likes': 12, 'views': 3},
+  {
+    'name': 'Pantalla verde de la muerte',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': 12,
+    'views': 3
+  },
   {
     'name': 'Pepperoni Pizza',
     'videoUrl': 'assets/videos/8.mp4',
