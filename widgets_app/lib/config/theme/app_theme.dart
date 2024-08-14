7985079df8a5849256c9dfe26f0e161712f1c636
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-14 14:54:29 +0200

    120: style: AppTheme and theme applied

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const colorlist = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.redAccent,
+  Colors.deepPurple,
+  Colors.purple,
+  Colors.pinkAccent
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected colour must be greater than 0'),
+        assert(selectedColor < colorlist.length,
+            'Selected colour must be less than ${colorlist.length}');
+
+  ThemeData getThemeData() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorlist[selectedColor],
+      );
+}
