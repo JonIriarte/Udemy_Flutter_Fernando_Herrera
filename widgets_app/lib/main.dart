3f10df017fefe5310d9c7cf48f58c38c4983756f
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-19 15:00:54 +0200

    feat: 123: add working links

--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/buttons/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -17,6 +19,11 @@ class MainApp extends StatelessWidget {
       home: const Scaffold(
         body: Center(child: HomeScreen()),
       ),
+      routes: <String, WidgetBuilder>{
+        '/home': (context) => const HomeScreen(),
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+      },
     );
   }
 }
