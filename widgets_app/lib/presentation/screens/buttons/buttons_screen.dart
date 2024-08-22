e6fe2e4bef199f662822fddb968dfe3348b98698
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-22 11:03:20 +0200

    feat: 126-127: buttons galore

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,21 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
 
+  static const String routeName = 'buttons';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Buttons Screen '),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new_rounded),
-            onPressed: () {
-              context.go('/');
-            },
-          ),
+      appBar: AppBar(
+        title: const Text('Buttons Screen'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            GoRouter.of(context).go(HomeScreen.routeName);
+          },
         ),
-        body: const Placeholder());
+      ),
+      body: const ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => GoRouter.of(context).go(HomeScreen.routeName),
+        child: const Icon(
+          Icons.arrow_back_ios_new_rounded,
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonsView extends StatelessWidget {
+  const ButtonsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: SizedBox(
+        width: double.infinity,
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Elevated button')),
+            const ElevatedButton(
+                onPressed: null, child: Text('Disabled button')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.access_alarm_rounded),
+                label: const Text('Elevated Icon')),
+            FilledButton(onPressed: () {}, child: const Text('Filled Button')),
+            FilledButton.icon(
+                onPressed: () {},
+                label: const Text('Icon filled'),
+                icon: const Icon(Icons.ac_unit_outlined)),
+            OutlinedButton(onPressed: () {}, child: const Text('Outlined')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                label: const Text('Icon'),
+                icon: const Icon(Icons.accessibility)),
+            TextButton(onPressed: () {}, child: const Text('Text button')),
+            TextButton.icon(
+                onPressed: () {},
+                label: const Text('Icon'),
+                icon: const Icon(Icons.account_circle_sharp)),
+            const CustomButton(),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.app_registration_outlined)),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_registration_outlined),
+              style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(colors.primary),
+                  iconColor: const WidgetStatePropertyAll(Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Material(
+          color: colors.primary,
+          child: InkWell(
+            onTap: () {},
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+              child:
+                  Text('Custom button', style: TextStyle(color: Colors.white)),
+            ),
+          )),
+    );
   }
 }
