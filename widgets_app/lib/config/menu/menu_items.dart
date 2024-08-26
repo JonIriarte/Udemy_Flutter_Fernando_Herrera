a1fb9a48593646d437153a1c219656b6b0d91372
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-08-26 11:42:30 +0200

    fix: Fix routes of card_screen and buttons_screen in menu_items

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -18,15 +18,20 @@ final appMenuItems = <MenuItem>[
       title: 'Botones',
       subtitle: 'Varios',
       iconData: Icons.smart_button_outlined,
-      link: '/buttons'),
+      link: '/buttons_screen'),
   const MenuItem(
       title: 'Tarjetas',
       subtitle: 'Un contenedor estilizado',
       iconData: Icons.credit_card,
-      link: '/cards'),
+      link: '/cards_screen'),
   const MenuItem(
       title: 'Tutorial',
       subtitle: 'Tutorial introductiorio',
       iconData: Icons.accessibility_new_rounded,
       link: '/app_tutorial_screen'),
+  const MenuItem(
+      title: 'Progress Indicators',
+      subtitle: 'Progress Indicators',
+      iconData: Icons.check_circle,
+      link: '/progress_screen')
 ];
