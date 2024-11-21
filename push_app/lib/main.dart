941801dd2e46a5e3f04d87f69f8fb332914dd42b
joiri1 <61544049+JonIriarte@users.noreply.github.com>
2024-11-21 11:19:36 +0100

    feat: new Project push_app project created

---
+++ push_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/config/router/app_router.dart';
+import 'package:push_app/config/theme/app_theme.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
+
+void main() {
+  runApp(MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => NotificationsBloc())],
+      child: const MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: true,
+      title: 'Flutter Demo',
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'HELLO',
+              ),
+            ],
+          ),
+        ));
+  }
+}
